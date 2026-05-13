54a227f3df728dec524212aa1406fad21a6e514c
dissatayev-a11y <d.issatayev@teez.kz>
2026-05-13 16:44:08 +0500

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 3000 });
+const port = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port });
 
 let rooms = [];
 let roomsByCode = {};
